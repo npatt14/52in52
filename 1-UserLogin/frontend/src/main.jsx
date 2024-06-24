c819278a84475791db77387d93a35989c3265dba
Nathan Patterson <imnathanpatterson@gmail.com>
2024-06-24 10:16:25 -0400

    finished login screen frontend, moving on to axios connection next

--- 1-UserLogin/frontend/src/main.jsx
+++ 1-UserLogin/frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'; // Import the Tailwind CSS file
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
